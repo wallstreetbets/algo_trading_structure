6513fd792681a06711bb82ea656520725bfcc8cb
CA Costa <11895361+wallstreetbets@users.noreply.github.com>
2019-01-30 00:46:25 -0500

    Create formulas.py

---
+++ formulas.py
@@ -0,0 +1,77 @@
+import numpy as np
+
+
+def rsiFunc(prices, n=14):
+    deltas = np.diff(prices)
+    seed = deltas[:n+1]
+    up = seed[seed >= 0].sum()/n
+    down = -seed[seed < 0].sum()/n
+    rs = up/down
+    rsi = np.zeros_like(prices)
+    rsi[:n] = 100. - 100./(1.+rs)
+
+    for i in range(n, len(prices)):
+        delta = deltas[i-1] # cause the diff is 1 shorter
+
+        if delta>0:
+            upval = delta
+            downval = 0.
+        else:
+            upval = 0.
+            downval = -delta
+
+        up = (up*(n-1) + upval)/n
+        down = (down*(n-1) + downval)/n
+
+        rs = up/down
+        rsi[i] = 100. - 100./(1.+rs)
+
+    return rsi
+
+
+def movingaverage(values,window):
+    weigths = np.repeat(1.0, window)/window
+    smas = np.convolve(values, weigths, 'valid')
+    return smas # as a numpy array
+
+
+def ExpMovingAverage(values, window):
+    weights = np.exp(np.linspace(-1., 0., window))
+    weights /= weights.sum()
+    a = np.convolve(values, weights, mode='full')[:len(values)]
+    a[:window] = a[window]
+    return a
+
+
+def computeMACD(x, slow=26, fast=12):
+    """
+    compute the MACD (Moving Average Convergence/Divergence) using a fast and slow exponential moving avg'
+    return value is emaslow, emafast, macd which are len(x) arrays
+    """
+    emaslow = ExpMovingAverage(x, slow)
+    emafast = ExpMovingAverage(x, fast)
+    MACD = emafast - emaslow
+    return MACD
+
+
+def VWAP(df):
+    price = (df['High'] + df['Low'] + df['Close']) / 3
+    vwap = (df['Volume'] * price).cumsum() / df['Volume'].cumsum()
+    return vwap
+
+
+def Bolinger_Bands(stock_price, window_size, num_of_std):
+
+    rolling_mean = stock_price.rolling(window=window_size).mean()
+    rolling_std = stock_price.rolling(window=window_size).std()
+    upper_band = rolling_mean + (rolling_std*num_of_std)
+    lower_band = rolling_mean - (rolling_std*num_of_std)
+
+    return rolling_mean, upper_band, lower_band
+
+
+def compute_daily_returns(df):
+    """Compute and return the daily return values."""
+    # Make a copy of the DataFrame
+    daily_returns = df.pct_change(1)
+    return daily_returns
