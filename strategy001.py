e19214904b5df520de972a2efbc88f5d74a16f47
CA Costa <11895361+wallstreetbets@users.noreply.github.com>
2019-01-31 02:17:45 -0500

    strategy001.py
    Strategy based on RSI, Moving Avg, Adj Price, Bollinger Bands

--- strategy001.py
+++ strategy001.py
@@ -3,43 +3,13 @@ import formulas
 from binance.client import Client
 from binance.websockets import BinanceSocketManager
 from datetime import datetime
+import conn
 import matplotlib.pyplot as plt
 import matplotlib
 matplotlib.use('TkAgg')
 
-PUBLIC_API_KEY = 'paste public key here'
-PRIVATE_API_KEY = 'paste private key here'
 
-client = Client(PUBLIC_API_KEY, PRIVATE_API_KEY)
-
-bm = BinanceSocketManager(client)
-
-symbol = "BTCUSDT"
-
-client = Client(PUBLIC_API_KEY, PRIVATE_API_KEY)
-data = client.get_historical_klines(symbol, Client.KLINE_INTERVAL_1HOUR, "{} day ago UTC".format(800))
-dates = []
-
-# Curate KLINE data, fix Datetime and delete useless cols
-for i in data:
-    timestamp = i[0] / 1000
-    timestamp = datetime.fromtimestamp(timestamp).strftime('%D')
-    dates.append(timestamp)
-    del i[0]
-    del i[-1]
-    del i[-1]
-    del i[-1]
-    del i[-1]
-    del i[-1]
-    del i[-1]
-
-# Create the data frame
-df = pd.DataFrame(data, index=dates, columns=["Open", "High", 'Low', 'Close', 'Volume'])
-df['Open'] = pd.to_numeric(df['Open'])
-df['High'] = pd.to_numeric(df['High'])
-df['Low'] = pd.to_numeric(df['Low'])
-df['Close'] = pd.to_numeric(df['Close'])
-df['Volume'] = pd.to_numeric(df['Volume'])
+df = conn.engine()
 
 # Calculate formulas and add cols
 df['Adj Price'] = (df['High'] + df['Low']) / 2
